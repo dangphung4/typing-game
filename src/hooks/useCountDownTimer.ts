b4381779bb6762d926742cd60846d7fb96fb6d66
dangphung4 <dangphung4@gmail.com>
2023-08-21 07:44:06 -0400

    add useeffect for counting

--- src/hooks/useCountDownTimer.ts
+++ src/hooks/useCountDownTimer.ts
@@ -1,26 +1,31 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 
-const useCountDownTimer = (seconds : number) => {
-    const [timeLeft, setTimeLeft] = useState(seconds);
-    const intervalRef = useRef<NodeJS.Timer | null>(null);
-    const hasTimerEnded = timeLeft <= 0;
-    const isRunning = intervalRef.current != null;
+const useCountDownTimer = (seconds: number) => {
+  const [timeLeft, setTimeLeft] = useState(seconds);
+  const intervalRef = useRef<NodeJS.Timer | null>(null);
+  const hasTimerEnded = timeLeft <= 0;
+  const isRunning = intervalRef.current != null;
 
-    const startCountdown = useCallback(() => {
+  const startCountdown = useCallback(() => {
+    intervalRef.current = setInterval(() => {
+      setTimeLeft((timeLeft) => timeLeft - 1);
+    }, 1000);
+  }, [setTimeLeft]);
 
-        intervalRef.current = setInterval(() => {
-            setTimeLeft((timeLeft) => timeLeft - 1);
-          }, 1000);
-    }, [setTimeLeft]);
+  const resetCountdown = useCallback(() => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+    }
+    setTimeLeft(seconds);
+  }, [seconds]);
+  useEffect(() => {
 
-    const resetCountdown = useCallback(() => {
-        if (intervalRef.current) {
-            clearInterval(intervalRef.current);
-        }
-        setTimeLeft(seconds);
-      }, [seconds]);
+    if(!timeLeft && intervalRef.current){
+        clearInterval(intervalRef.current);
+    }
+  }, [timeLeft,intervalRef]);
 
-      return { timeLeft, startCountdown, resetCountdown };
-}
+  return { timeLeft, startCountdown, resetCountdown };
+};
 
 export default useCountDownTimer;
