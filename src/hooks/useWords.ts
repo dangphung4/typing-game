0450529ea95dc2c23ad94365654903a39cdc7ad2
dangphung4 <dangphung4@gmail.com>
2023-08-21 07:29:22 -0400

    implememnt use of word engine

--- src/hooks/useWords.ts
+++ src/hooks/useWords.ts
@@ -1,3 +1,18 @@
-const useWords = () => {
-    
-}
+import { faker } from "@faker-js/faker";
+import { useCallback, useState } from "react";
+
+const generateWords = (count: number) => {
+    return faker.word.words(count).toLowerCase();
+  };
+
+const useWords = (count: number) => {
+    const [words, setWords] = useState<string>(generateWords(count));
+  
+    const updateWords = useCallback(() => {
+      setWords(generateWords(count));
+    }, [count]);
+  
+    return { words, updateWords };
+  };
+
+export default useWords;
