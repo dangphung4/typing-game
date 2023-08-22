a9eb4d39e46e2158f20817a1f81ceddd59222609
dangphung4 <dangphung4@gmail.com>
2023-08-22 06:13:39 -0400

    add typing functionality hook

--- src/hooks/userEngine.ts
+++ src/hooks/userEngine.ts
@@ -1,20 +1,22 @@
 import { useCallback, useEffect, useState } from "react";
 import useWords from "./useWords";
 import useCountDownTimer from "./useCountDownTimer";
-
-
+import useTypings from "./useTypings";
 
 export type State = "start" | "run" | "finish";
 
 const NUMBER_OF_WORDS = 12;
 const COUNTDOWN_SECONDS = 30;
 
-const useEngine = () =>{
-    const [state, setState] = useState<State>("start");
-    const {words, updateWords} = useWords(NUMBER_OF_WORDS);
-    const { timeLeft, startCountdown, resetCountdown } = useCountDownTimer(COUNTDOWN_SECONDS);
+const useEngine = () => {
+  const [state, setState] = useState<State>("start");
+  const { words, updateWords } = useWords(NUMBER_OF_WORDS);
+  const { timeLeft, startCountdown, resetCountdown } =
+    useCountDownTimer(COUNTDOWN_SECONDS);
+  const { typed, cursor, clearTyped, resetTotalTyped, totalTyped } =
+    useTypings(state !== "finish");
 
-    return {state, words, timeLeft};
-}
+  return { state, words, timeLeft };
+};
 
 export default useEngine;
